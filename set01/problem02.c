f27c7b1cdbcbf16a8c369db276149ba8c406eeb3
Sadhvi  J <115769589+sadhvij19@users.noreply.github.com>
2022-12-07 09:16:02 +0000

    problem02 is done

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int n1,n2,sum;
+  printf("Enter the number of n1:");
+  scanf("%d",&n1);
+  printf("Enter the number of n2:");
+  scanf("%d",&n2);
+  sum=n1+n2;
+  printf("sum=%d",sum);
+}
