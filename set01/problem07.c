e46f87e0c9d7005c48346549b537b07d32da55a3
Sadhvi  J <115769589+sadhvij19@users.noreply.github.com>
2022-12-07 09:03:53 +0000

    problem07 is done

---
+++ set01/problem07.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+  int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+
+int input_n()
+{
+ int n;
+  printf("enter the value \n");
+  scanf("%d",&n);
+  return n;
+  }
+int sum_n_nos(int n)
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+    {
+      sum=sum+i;
+      }
+      return sum;
+    }
+
+
+  void output(int n,int sum)
+  {
+    printf("the sum of %d is %d/n",n,sum);
+  }  
+
+int main()
+{
+  int n,sum=0;
+  n=input_n();
+  sum=sum_n_nos(n);
+  output(n, sum);
+  }
